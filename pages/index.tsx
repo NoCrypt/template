101972c6225cfa93fb903d1b5bad9a4a45212e19
NoCrypt <aldyhkmz@gmail.com>
2022-06-16 22:44:30 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -15,8 +14,8 @@ const Home: NextPage = () => {
         <p className="pt-5">
           This is my nextjs template for rapidly creating apps. <br/>
           It contains: <code>tailwindcss, @fontsource/roboto-condensed, next-themes</code> <br/>
-          What's configured? black themed by default, no system pref theme, no flasing, font configured, next themes ready, using pnpm, custom documents configured, components folder setup, 404 custom error.
-
+          What's configured? dark themed by default, no system pref theme, no flashing, offline font configured, next themes ready, 
+          using pnpm, custom documents configured, components folder setup, 404 custom error.
         </p>
       </main>
     </div>
